c71755a6358da5cfa7839d0f04d2f90e9e1c0b8e
Sneha-0127 <snehamk27112005@gmail.com>
2025-04-22 09:29:30 +0530

    addition

---
+++ Add.java
@@ -0,0 +1,10 @@
+package Simple_java;
+
+public class Add {
+public static void main(String[] args){
+    int a=12;
+    int b=27;
+    int sum=a+b;
+    System.out.println("addition of 12 and 27 is"+sum);
+}
+}
